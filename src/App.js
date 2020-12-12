802ea5fe10df5e4d8b71e9f32dc4a06ce7e885b8
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-12 17:09:46 +0530

    adding payload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
-import CakeContainer from './Components/CakeContainer';
-import IceCreamContainer from './Components/IceCreamContainer';
+import NewCakeContainer from './Components/NewCakeContainer';
 
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
-        <CakeContainer/>
-        <IceCreamContainer/>
+        <NewCakeContainer/>
       </div>
     </Provider>
 
