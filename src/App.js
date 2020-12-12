6481b732f6fb92df16f9066e03598d18cb60f7ce
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-12 11:42:44 +0530

    store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import CakeContainer from './Components/CakeContainer';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer/>
-    </div>
+    <Provider>
+      <div className="App">
+        <CakeContainer/>
+      </div>
+    </Provider>
+
   );
 }
 
