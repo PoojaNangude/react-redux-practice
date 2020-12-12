57e34f41066339c7e1ecdeeea52765dac0ff8e83
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-12-12 13:29:59 +0530

    useDispatch Hook

--- src/Components/HooksCakeContainer.js
+++ src/Components/HooksCakeContainer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 //to get hold of nay state that is maintained in the redux store we use the useSelector hook
 //performs the same function as mapStateToProps
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { buyCake } from '../Redux';
 
 function HooksCakeContainer() {
     const numOfCakes = useSelector(state => state.numOfCakes) //this functions receives the redux state as the argument like mapStateToProps
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Number of Cakes : {numOfCakes}</h2>
-            <button>Buy Cake</button>
+            <button onClick = {() => dispatch(buyCake())}>Buy Cake</button>
         </div>
     )
 }
